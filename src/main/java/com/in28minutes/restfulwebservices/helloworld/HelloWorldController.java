83de551aa703ef9d5c0e9fb42ecdde56f2f05e82
shinichi-ito <shinichi-ito@lotsjoys.com>
2020-05-14 22:18:49 +0900

    Internationalization for RESTful Services part2

--- src/main/java/com/in28minutes/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28minutes.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -32,6 +33,6 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-internationalized")
     public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
 
-        return messageSource.getMessage("good.morning.message",null, locale);
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
     }
 }
