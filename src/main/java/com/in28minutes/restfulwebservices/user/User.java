576a43619a6e87046cd20520729bef8e05328baa
shinichi-ito <shinichi-ito@lotsjoys.com>
2020-05-26 07:41:00 +0900

    Implementing Dynamic Filtering for RESTful Service

--- src/main/java/com/in28minutes/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/restfulwebservices/user/User.java
@@ -1,15 +1,21 @@
 package com.in28minutes.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user.")
 public class User {
     private Integer id;
 
     @Size(min=2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes = "Name should have atleast 2 characters")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Bitrh date should be in the past")
     private Date birthDate;
 
     public Integer getId() {
